e0fdcd01e5cb5a703c11dfa54d42a09d0916eb86
som-syom <dhyeon@student.42seoul.kr>
2021-05-28 22:40:10 +0900

    Feat - dockerfile, mysql

--- inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -4,5 +4,7 @@ echo "flush privileges;
 create database if not exists wordpress;
 grant all privileges on *.* to 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD';
 create user '$MYSQL_USER' identified by '$MYSQL_PASSWORD';
-grant all privileges on wordpress.* to '$MYSQL_USER';" | mysqld -u root --datadir=/var/lib/mysql --bootstrap
+grant all privileges on wordpress.* to '$MYSQL_USER';" | mysqld -u root --datadir=/var/lib/mysql --bootstrap 
+mysql < wp.sql
+service mariadb stop
 mysqld -u root
--- inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -8,6 +8,5 @@ rm wordpress-5.5.1.tar.gz
 echo -e "\nenv[MYSQL_ROOT_PASSWORD] = $MYSQL_ROOT_PASSWORD" >> /etc/php/7.3/fpm/pool.d/www.conf
 echo -e "\nenv[MYSQL_USER]=$MYSQL_USER" >> /etc/php/7.3/fpm/pool.d/www.conf
 echo -e "\nenv[MYSQL_PASSWORD]=$MYSQL_PASSWORD" >> /etc/php/7.3/fpm/pool.d/www.conf
-# service php7.3-fpm start
-# sleep infinity
+
 php-fpm7.3 -F
