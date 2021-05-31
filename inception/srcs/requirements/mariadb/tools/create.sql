b3a67ef7d4d940cbc8fb48c83b6395e785b28c95
som-syom <dhyeon@student.42seoul.kr>
2021-05-31 22:25:28 +0900

    Feat - mysql user setting

--- inception/srcs/requirements/mariadb/tools/create.sql
+++ inception/srcs/requirements/mariadb/tools/create.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
-GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'%' identified by 'password' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON wordpress.* TO 'dhyeon'@'%' identified by 'dhyeon' WITH GRANT OPTION;
 create user 'root' identified by 'password';
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
 grant all privileges on wordpress.* to 'root';
