c9e2b433a007a0b1f62eaaa176e8abf949c7c05f
som-syom <noenna@naver.com>
2021-06-02 03:00:46 +0900

    Feat - user 호스트 '%'에서 localhost 로 변경

--- inception/srcs/requirements/mariadb/tools/create.sql
+++ inception/srcs/requirements/mariadb/tools/create.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
 create user 'dhyeon' identified by 'password';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'dhyeon'@'%' identified by 'password' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON wordpress.* TO 'dhyeon'@'localhost' identified by 'password';
 create user 'root' identified by 'password';
 flush privileges;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
