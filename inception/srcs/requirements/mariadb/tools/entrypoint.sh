329571ba142203fa3bc839a6410869f3f5632b88
som-syom <dhyeon@student.42seoul.kr>
2021-06-01 22:33:23 +0900

    Feat - sql backup

--- inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,7 +1,9 @@
 /etc/init.d/mariadb setup
 service mariadb start
-sleep 5
+sleep 3
 mysql -u root --password=$MYSQL_ROOT_PASSWORD < create.sql
+# mysql -u root --password=$MYSQL_ROOT_PASSWORD < wp_users.sql
+# mysql -u root --password=$MYSQL_ROOT_PASSWORD < wp_usermeta.sql
 mysql -u root --password=$MYSQL_ROOT_PASSWORD < wp.sql
 service mariadb stop
 /usr/bin/mysqld --datadir="/var/lib/mysql"
