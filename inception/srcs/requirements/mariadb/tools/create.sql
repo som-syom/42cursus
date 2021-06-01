08b82636a5b1ec6a0a53abfb9e837abe7f887ab3
som-syom <dhyeon@student.42seoul.kr>
2021-06-01 18:15:37 +0900

    Feat - Makefile 수정, 비밀번호 받고 들어가도록 수정

--- inception/srcs/requirements/mariadb/tools/create.sql
+++ inception/srcs/requirements/mariadb/tools/create.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
-GRANT ALL PRIVILEGES ON wordpress.* TO 'dhyeon'@'%' identified by 'dhyeon' WITH GRANT OPTION;
+create user 'dhyeon' identified by 'password';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'dhyeon'@'%' identified by 'password' WITH GRANT OPTION;
 create user 'root' identified by 'password';
+flush privileges;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
 grant all privileges on wordpress.* to 'root';
 flush privileges;
--- inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,8 +1,7 @@
-
 /etc/init.d/mariadb setup
 service mariadb start
 sleep 5
-mysql < create.sql
-mysql < wp.sql
+mysql -u root --password=$MYSQL_ROOT_PASSWORD < create.sql
+mysql -u root --password=$MYSQL_ROOT_PASSWORD < wp.sql
 service mariadb stop
 /usr/bin/mysqld --datadir="/var/lib/mysql"
