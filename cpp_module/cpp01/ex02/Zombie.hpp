4741906a1ae98b78f1955e3a1547bba649cd0529
som-syom <dhyeon@student.42seoul.kr>
2021-06-21 17:11:29 +0900

    Add - cpp01 ex02 파일 추가

---
+++ cpp_module/cpp01/ex02/Zombie.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhyeon <dhyeon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/21 16:53:19 by dhyeon            #+#    #+#             */
+/*   Updated: 2021/06/21 17:05:29 by dhyeon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <string>
+# include <iostream>
+
+class Zombie
+{
+	private:
+		std::string	name;
+		std::string	type;
+	public:
+		Zombie(/* args */);
+		~Zombie();
+		void	announce(void);
+};
+
+#endif
---
+++ cpp_module/cpp01/ex02/ZombieEvent.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ZombieEvent.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhyeon <dhyeon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/21 16:53:25 by dhyeon            #+#    #+#             */
+/*   Updated: 2021/06/21 17:07:22 by dhyeon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIEEVENT_HPP
+# define ZOMBIEEVENT_HPP
+
+# include <string>
+# include <iostream>
+
+class ZombieEvent
+{
+	private:
+		/* data */
+	public:
+		ZombieEvent(/* args */);
+		~ZombieEvent();
+};
+
+#endif
