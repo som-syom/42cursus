2db27fc420d459099a48d1b8ccce93ee8de877cc
som-syom <dhyeon@student.42seoul.kr>
2021-06-23 16:30:48 +0900

    Feat - main 수정

---
+++ cpp_module/cpp01/ex06/Weapon.cpp
@@ -0,0 +1,26 @@
+#include "Weapon.hpp"
+
+Weapon::Weapon()
+{
+	this->type = "nothing";
+}
+
+Weapon::Weapon(std::string type)
+{
+	this->type = type;
+}
+
+Weapon::~Weapon()
+{
+	
+}
+
+const std::string&	Weapon::getType()
+{
+	return (this->type);
+}
+
+void	Weapon::setType(const std::string &type)
+{
+	this->type = type;
+}
