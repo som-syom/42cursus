a71269eaa1718327b6d967a0f9b8c54d840b2b11
som-syom <dhyeon@student.42seoul.kr>
2021-06-24 22:27:09 +0900

    Feat - cpp ex07 done

--- cpp_module/cpp01/ex07/main.cpp
+++ cpp_module/cpp01/ex07/main.cpp
@@ -4,20 +4,35 @@
 
 int		main(int argc, char **argv)
 {
-	std::fstream	s1;
-	std::fstream	s2;
-	std::string		name;
+	std::ifstream	s1;
+	std::ofstream	s2;
 	std::string		line;
+	std::size_t		it = 0, idx = 0;
+	char			*buf = 0;
+	int				size;
 
-	(void)argc;
-	(void)argv;
-	// if (argc != 4)
-	// 	return (0);
-	s1.open(argv[1], std::fstream::in);
-	name.append(argv[1]);
-	name.append(".replace");
-	s2.open(name, std::fstream::out);
-	
+	if (argc != 4 || !std::strlen(argv[2]))
+		return (1);
+	s1.open(argv[1], std::fstream::in | std::fstream::ate);
+	if (!s1.is_open())
+	{
+		std::cout << "open error" << std::endl;
+		return (1);
+	}
+	s2.open(std::string(argv[1]) + ".replace", std::fstream::out);
+	size = s1.tellg();
+	buf = new char[size];
+	s1.seekg(0);
+	s1.read(buf, size);
+	line = buf;
+	while ((it = line.find(argv[2], idx)) != std::string::npos)
+	{
+		line.replace(it, std::strlen(argv[2]), argv[3]);
+		idx = it + std::strlen(argv[3]);
+	}
+	s2 << line;
+	delete[] buf;
 	s1.close();
 	s2.close();
+	return (0);
 }
