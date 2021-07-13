dcf676c8ec3fc1be6ad28fb575d374e7f2ddd41e
som-syom <dhyeon@student.42seoul.kr>
2021-07-13 16:54:49 +0900

    Feat - enemy 메서드 추가

--- cpp_module/cpp04/ex01/Enemy.cpp
+++ cpp_module/cpp04/ex01/Enemy.cpp
@@ -4,21 +4,23 @@ Enemy::Enemy(int hp, std::string const& type)
 {
 	this->type = type;
 	this->hp = hp;
+	std::cout << "Enemy" << this->type << " is created" << std::endl;
 }
 
 Enemy::~Enemy()
 {
-
+	std::cout << "Enemy" << this->type << " is removed" << std::endl;
 }
 
 Enemy::Enemy(Enemy const& origin)
 {
-
+	*this = origin;
 }
 
 Enemy& Enemy::operator=(Enemy const& origin)
 {
-
+	this->hp = origin.hp;
+	this->type = origin.type;
 }
 
 void	takeDamage(int)
