533926681a9daec0f01b0a143632f36192423a6b
som-syom <dhyeon@student.42seoul.kr>
2021-08-13 14:22:32 +0900

    Feat - ex01

--- cpp_module/cpp08/ex01/span.cpp
+++ cpp_module/cpp08/ex01/span.cpp
@@ -1,23 +1,63 @@
 #include "span.hpp"
 
-span::span()
+Span::Span()
+{
+}
+
+Span::Span(unsigned int N)
+{
+	this->_size = N;
+	if (static_cast<int>(N) < 0)
+		throw Span::OutofRangeException();
+}
+
+Span::~Span()
+{
+	this->_numbers.clear();
+}
+
+Span::Span(Span const& origin)
+{
+	this->_N = origin._N;
+	this->_count = origin._count;
+	this->_numbers = new int[this->_N];
+	for (int i = 0; i < this->_count; i++)
+		this->_numbers[i] = origin._numbers[i];
+}
+
+Span& Span::operator=(Span const& origin)
+{
+	if (!this->_numbers)
+		delete[] this->_numbers;
+	this->_N = origin._N;
+	this->_count = origin._count;
+	this->_numbers = new int[this->_N];
+	for (int i = 0; i < this->_count; i++)
+		this->_numbers[i] = origin._numbers[i];
+	return (*this);
+}
+
+void Span::addNumber(unsigned int num)
+{
+	for (int i = 0; i < this->_count; i++)
+	{
+		if (this->_numbers[i] == num)
+		{
+			throw DuplicatedNumException();
+			return ;
+		}
+	}
+	this->_numbers[this->_count] = num;
+	this->_count++;
+}
+
+int Span::shortestSpan()
 {
 	
 }
 
-span::~span()
+int Span::longestSpan()
 {
 
-}
-
-span::span(span const& origin)
-{
-	*this = origin;
-}
-
-span& span::operator=(span const& origin)
-{
-	this->value = origin.value;
-	return (*this);
 }
 
--- cpp_module/cpp08/ex01/span.hpp
+++ cpp_module/cpp08/ex01/span.hpp
@@ -3,16 +3,44 @@
 
 # include <iostream>
 # include <string>
+# include <set>
 
-class span
+class Span
 {
 	private:
-		unsigned int	_N;
+		unsigned int _size;
+		std::set<int> _numbers;
+		Span();
 	public:
-		span();
-		~span();
-		span(span const&);
-		span& operator=(span const&);
+		Span(unsigned int);
+		virtual ~Span();
+		Span(Span const&);
+		Span& operator=(Span const&);
+
+		void addNumber(unsigned int);
+		int shortestSpan();
+		int longestSpan();
+
+		class OutofRangeException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+		class DuplicatedNumException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+		class NotFoundNumException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+		class NumCountOneException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
 
 };
 
