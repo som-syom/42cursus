c05c5f2aa37ef81c0088e7dcec2cdf7cc53485b4
som-syom <dhyeon@student.42seoul.kr>
2021-08-13 14:23:40 +0900

    Feat - span cpp 수정

--- cpp_module/cpp08/ex01/span.cpp
+++ cpp_module/cpp08/ex01/span.cpp
@@ -18,37 +18,17 @@ Span::~Span()
 
 Span::Span(Span const& origin)
 {
-	this->_N = origin._N;
-	this->_count = origin._count;
-	this->_numbers = new int[this->_N];
-	for (int i = 0; i < this->_count; i++)
-		this->_numbers[i] = origin._numbers[i];
+	
 }
 
 Span& Span::operator=(Span const& origin)
 {
-	if (!this->_numbers)
-		delete[] this->_numbers;
-	this->_N = origin._N;
-	this->_count = origin._count;
-	this->_numbers = new int[this->_N];
-	for (int i = 0; i < this->_count; i++)
-		this->_numbers[i] = origin._numbers[i];
-	return (*this);
+	
 }
 
 void Span::addNumber(unsigned int num)
 {
-	for (int i = 0; i < this->_count; i++)
-	{
-		if (this->_numbers[i] == num)
-		{
-			throw DuplicatedNumException();
-			return ;
-		}
-	}
-	this->_numbers[this->_count] = num;
-	this->_count++;
+	
 }
 
 int Span::shortestSpan()
